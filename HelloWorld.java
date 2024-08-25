443911c1434502b35313e87db58d1fbe18d54bc4
harrellhai <hoanghai231022@gmai.com>
2024-08-25 21:16:41 +0700

    init project

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package learningjava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world with java and hỏi dân it ");
+		System.out.println("Hello world with java and hỏi dân it ");
+		System.out.println("Hello world with java and hỏi dân it ");
+		System.out.println("Hello world with java and hỏi dân it ");
+		System.out.println("Hello world with java and hỏi dân it ");
+
+	}
+
+}
